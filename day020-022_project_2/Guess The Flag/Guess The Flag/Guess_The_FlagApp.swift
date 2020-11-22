f11d76a580e0c592d5408e6f2517285c64179595
mattszeto <ms8288@gmail.com>
2020-11-21 23:28:50 -0800

    Day 20 - 22: Project 2 -> Guess The Flag: Guessing flag game to learn stacks, and styling

---
+++ day020-022_project_2/Guess The Flag/Guess The Flag/Guess_The_FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_The_FlagApp.swift
+//  Guess The Flag
+//
+//  Created by Matt on 11/21/20.
+//
+
+import SwiftUI
+
+@main
+struct Guess_The_FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
