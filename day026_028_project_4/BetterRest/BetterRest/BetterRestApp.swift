e28b9da57372d379a11f456c245839fe1b2e6beb
mattszeto <ms8288@gmail.com>
2020-11-23 19:29:26 -0800

    Day 26 - 28: Project 4 -> BetterRest: a sleep time calculator using coreML, Steppers, and Date

---
+++ day026_028_project_4/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Matt on 11/23/20.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
