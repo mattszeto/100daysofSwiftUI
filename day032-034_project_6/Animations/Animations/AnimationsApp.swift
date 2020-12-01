07b16e34c0ca037fcbae4baafe6b44d5b6c856d6
mattszeto <ms8288@gmail.com>
2020-11-30 16:56:24 -0800

    Changed folder names -> Created Project 6 for days 32 - 34

---
+++ day032-034_project_6/Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Matt on 11/30/20.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
